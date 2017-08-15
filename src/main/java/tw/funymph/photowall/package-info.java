ddea9d4d58dd28954c2b8529e652a1b67166e266
Spirit <dbi1463@gmail.com>
2017-08-15 23:23:33 +0800

    Add Vert.x dependency.

---
+++ src/main/java/tw/funymph/photowall/package-info.java
@@ -0,0 +1,15 @@
+/* package-info.java created on Aug 6, 2017.
+ * 
+ * Copyright (C) Funymph all rights reserved.
+ *
+ * This file is a part of the PhotoWall Vert.x project.
+ */
+
+/**
+ * This package contains all interfaces and classes of PhotoWall.
+ * 
+ * @author Spirit Tu
+ * @version 1.0
+ * @since 1.0
+ */
+package tw.funymph.photowall;
